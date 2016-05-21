37265541822ada6bbc6c57c0fe55d8aa91edef32
leolin310148 <leolin310148@gmail.com>
2016-05-21 15:42:53 +0800

    take picture

--- TibameShopping/app/src/main/java/com/tibame/shopping/NewItemActivity.java
+++ TibameShopping/app/src/main/java/com/tibame/shopping/NewItemActivity.java
@@ -1,8 +1,12 @@
 package com.tibame.shopping;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -13,11 +17,30 @@ public class NewItemActivity extends AppCompatActivity {
     }
 
     public void takePicture(View view) {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1001 );
+
     }
 
     public void pickPicture(View view) {
     }
 
     public void doAddItem(View view) {
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1001) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                Bitmap bitmap = (Bitmap) extras.get("data");
+                ImageView imageViewItemPicture = (ImageView) findViewById(R.id.imageViewItemPicture);
+                imageViewItemPicture.setImageBitmap(bitmap);
+            }
+
+        }
     }
 }
