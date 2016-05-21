f1f8899408a65146852a7121349917f592fc529e
leolin310148 <leolin310148@gmail.com>
2016-05-21 11:51:08 +0800

    main layout & LoginActivity

--- TibameShopping/app/src/main/java/com/tibame/shopping/MainActivity.java
+++ TibameShopping/app/src/main/java/com/tibame/shopping/MainActivity.java
@@ -1,13 +1,14 @@
 package com.tibame.shopping;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,21 +17,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
 
-        database.getReference().child("name")
-                .addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        TextView textView = (TextView) findViewById(R.id.textView);
-                        textView.setText(dataSnapshot.getValue().toString());
-                    }
+        FirebaseUser currentUser = auth.getCurrentUser();
 
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+        TextView textViewCurrentUserName = (TextView) findViewById(R.id.textViewCurrentUserName);
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
 
-                    }
-                });
+        if (currentUser == null) {
+            textViewCurrentUserName.setText(  "還沒有登入"  );
+            buttonLogin.setText(  "登入"  );
+        } else {
+            textViewCurrentUserName.setText(  currentUser.getEmail()  );
+            buttonLogin.setText(  "登出"  );
+        }
+
+    }
+
+    public void doLogin(View view) {
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser == null) {
+
+            // 到登入頁面去
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+
+        } else {
+            // 登出
+            auth.signOut();
+
+        }
 
     }
 }
