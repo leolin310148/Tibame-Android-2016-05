562ea1d85038b721a6f3986eefe5106c7f222d5f
leolin310148 <leolin310148@gmail.com>
2016-05-21 15:26:03 +0800

    new item layout

--- TibameShopping/app/src/main/java/com/tibame/shopping/NewItemActivity.java
+++ TibameShopping/app/src/main/java/com/tibame/shopping/NewItemActivity.java
@@ -2,6 +2,7 @@ package com.tibame.shopping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewItemActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class NewItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+    }
+
+    public void takePicture(View view) {
+    }
+
+    public void pickPicture(View view) {
+    }
+
+    public void doAddItem(View view) {
     }
 }
